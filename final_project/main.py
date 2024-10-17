fc76b410ff37ea1b28a1159d6409e97a51bf0066
Josué Barrios Ramírez <130526195+JosBar765@users.noreply.github.com>
2024-10-16 22:44:07 -0600

    Update main.py
    Agregué los iconos dentro del frame de la cámara

--- final_project/main.py
+++ final_project/main.py
@@ -1,12 +1,13 @@
 import cv2 
 from cvzone.HandTrackingModule import HandDetector 
 import serial
+import subprocess
 
 COMPORT='COM7' # This comport can change eventually
 ARDUINO = serial.Serial(COMPORT, 9600) # Baud rate always 9600
 DETECTOR=HandDetector(detectionCon=0.8,maxHands=1) 
-CAMERA=1 # Cameras are stored in a vector
-VIDEO=cv2.VideoCapture(CAMERA)
+# Cameras are stored in a vector (Index 0 it's the default camera)
+VIDEO=cv2.VideoCapture(1) if cv2.VideoCapture(1).isOpened() else cv2.VideoCapture(0)
 
 def isHandFlipped(handLandmarkList): 
     # Consider 'flipped' as the back of the hand 
@@ -38,16 +39,16 @@ def moveCar(fingerUp):
         ARDUINO.write(b'S')
 
 def steeringIndication(fingerUp): 
-    if fingerUp==[0,0,0,0,0]: 
-        return "Adelante" 
-    elif fingerUp==[1,0,0,0,1]: 
-        return "Izquierda" 
-    elif fingerUp==[0,1,1,0,0]: 
-        return "Derecha" 
-    elif fingerUp==[1,1,1,1,1]: 
-        return "Atras" 
-    else: 
-        return "Parar"
+    if fingerUp == [0, 0, 0, 0, 0]:
+        return "Adelante", "icons/adelante.jpg"
+    elif fingerUp == [1, 0, 0, 0, 1]:
+        return "Izquierda", "icons/izquierda.jpg"
+    elif fingerUp == [0, 1, 1, 0, 0]:
+        return "Derecha", "icons/derecha.jpg"
+    elif fingerUp == [1, 1, 1, 1, 1]:
+        return "Atras", "icons/atras.jpg"
+    else:
+        return "Parar", "icons/parar.png"
 
 def printDebugMessages(landmarkList):
     print(f"Dedos de la mano: {fingersUp(landmarkList)}") 
@@ -55,7 +56,12 @@ def printDebugMessages(landmarkList):
     print(f"Acción carrito: {steeringIndication(fingersUp(landmarkList))}")
     print("-----------------------------------------------------------------------------------")
 
-if __name__ == "__main__": 
+def clearConsole():
+    subprocess.run("cls", shell=True)
+
+if __name__ == "__main__":
+    clearConsole()
+    
     while True: 
         ret,frame=VIDEO.read() 
         hands,img=DETECTOR.findHands(frame, draw=True) 
@@ -63,17 +69,25 @@ if __name__ == "__main__":
         if hands: 
             landmarkList=hands[0]
             fingerUp=fingersUp(landmarkList) 
+            direction, icon_path = steeringIndication(fingerUp)
 
-            cv2.putText(frame,f'Direccion: {steeringIndication(fingerUp)}',(10,450),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255,255,255),1,cv2.LINE_AA) 
+            cv2.putText(frame, f'Direccion: {direction}', (10, 450), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 1, cv2.LINE_AA)
+            icon=cv2.imread(icon_path)
+            icon_dimensional=100
+            icon = cv2.resize(icon, (icon_dimensional, icon_dimensional))
+            icon_position=[10, 300] # [vertical_value, horizontal_value]
+            frame[icon_position[0]:icon_position[0]+icon_dimensional, icon_position[1]:icon_position[1]+icon_dimensional] = icon
 
             moveCar(fingerUp) 
-
+            
             printDebugMessages(landmarkList)
 
-        cv2.imshow("VIDEO",frame) 
+        cv2.imshow("VIDEO",frame)
         key=cv2.waitKey(1) 
         if key==27: # Key 27 ASCII ('Esc') 
+            ARDUINO.write(b'X') # Turn off all the leds
             break 
 
     VIDEO.release() 
     cv2.destroyAllWindows()
+    clearConsole()
